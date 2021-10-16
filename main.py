2516ac17f7dc272b3b8c0ec2ef54683c4041aa98
Dewmal <dewmal@egreen.io>
2021-10-16 15:38:06 +0530

    all, search

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import sys
 import json
 import os
+from pprint import pprint
 
-__session_file__ = "db/session.db"
-__item_folder__ = "db/item"
-__item__last_id__ = "db/item_id.db"
+__db_location__ = "db"
+__session_file__ = f"{__db_location__}/session.db"
+__item_folder__ = f"{__db_location__}/item"
+__item__last_id__ = f"{__db_location__}/item_id.db"
+
+
+def init(arguments):
+
+    def db():
+        os.makedirs(__item_folder__)
+
+    section = arguments[0]
+    if section == "init":
+        command = arguments[1]
+        if command == "db":
+            db()
 
 
 def __get_logged_user():
@@ -51,12 +65,41 @@ class Item:
             f.write(str(self.last_id))
 
     def find(self, id):
-        with open(f"{__item_folder__}/{id}.db", "r") as item_file:
+        Item.__get_item_by_path(self, f"{__item_folder__}/{id}.db")
+
+    def __get_item_by_path(item, path):
+        with open(path, "r") as item_file:
             _data_ = json.load(item_file)
-            self.id = _data_["id"]
-            self.name = _data_["name"]
-            self.price = _data_["price"]
-            self.selling_price = _data_["sellingPrice"]
+            item.id = _data_["id"]
+            item.name = _data_["name"]
+            item.price = _data_["price"]
+            item.selling_price = _data_["sellingPrice"]
+
+    def all(self):
+        item_file_names = os.listdir(__item_folder__)
+        items = []
+        for item_file_name in item_file_names:
+            item = Item()
+            Item.__get_item_by_path(
+                item, f"{__item_folder__}/{item_file_name}")
+            items.append(item)
+        return items
+
+    def search(self, key, value):
+        items = self.all()
+        result_items = []
+        for item in items:
+            item_value = getattr(item,key)
+            if item_value == value:
+                result_items.append(item)
+        return result_items
+
+
+    def __repr__(self):
+        return f"id:{self.id},name:{self.name},price:{self.price}"
+
+    def __str__(self):
+        return f"id:{self.id},name:{self.name},price:{self.price}"
 
 
 def item_create(name, price, selling_price):
@@ -68,17 +111,25 @@ def item_create(name, price, selling_price):
 
 
 def item_all():
-    print("Item All")
-
+    item = Item()
+    items = item.all()
+    pprint(items)
 
 def item_view(id):
     item = Item()
     item.find(id)
     print(item.id, item.name, item.price, item.selling_price)
 
+def item_search(key,value):
+    item = Item()
+    results = item.search(key,value)
+    pprint(results)
+
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
+
+    init(arguments)
 
     section = arguments[0]
     command = arguments[1]
@@ -96,3 +147,6 @@ if __name__ == "__main__":
             item_all()
         elif command == "view":
             item_view(*params)
+        elif command == "search":
+            item_search(*params)
+
